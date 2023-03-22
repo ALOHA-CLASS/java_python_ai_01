7fe9057023f8bd515960fefd323f4fff24089042
wwwalohacampus <h850415@naver.com>
2023-03-22 10:39:53 +0900

    init

---
+++ project/src/main/java/com/human/project/ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.human.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
---
+++ project/src/main/java/com/human/project/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.human.project;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjectApplication.class);
+	}
+
+}
