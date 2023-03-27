20b897fbaacbe2336056294b5a4c9b3444036957
Depra3 <parkmin0318@gmail.com>
2023-03-27 09:27:32 +0900

    관리자 권한 관리 [ 게시글 다중 선택 및 삭제, 유저 다중 삭제 ]

--- project/src/main/java/com/human/project/controller/BoardController.java
+++ project/src/main/java/com/human/project/controller/BoardController.java
@@ -111,14 +111,12 @@ public class BoardController {
 	}
 	
 	// 게시글 삭제
-	@PostMapping("/delete")
-	public String delete(int boardNo) throws Exception {
+	public String delete(@RequestParam(value="chkbox[]") List<String> boardNoList) throws Exception {
 		
-		int result = boardService.delete(boardNo);
+		int result = boardService.delete(boardNoList);
 		
-		if( result > 0 ) 	log.info("게시글 삭제 성공...");
-		else 				log.info("게시글 삭제 실패...");
-
+		if (result > 0) log.info("게시글 삭제 성공");
+		else 			log.info("게시글 삭제 실패");
 		
 		return "redirect:/board/list";
 	}
