015d5490af7b100b39202bcbf146c0778dbb5d5d
ohtaekyun <ohtaekyun@naver.com>
2023-03-30 09:39:05 +0900

    ohtaekyun 2023-03-30 09:39

--- project/src/main/java/com/human/project/controller/HomeController.java
+++ project/src/main/java/com/human/project/controller/HomeController.java
@@ -160,14 +160,14 @@ public class HomeController {
 	}
 	
     //아이디&비밀번호 찾기
-	@PostMapping("/find_id")
-	public ResponseEntity<List> findId(Users user) throws Exception {    	
-		Users selectedUser = userService.selectByEmail(user);
-		String findId = selectedUser.getUserId();
-		String findName = selectedUser.getName();
-		List<String> result = Arrays.asList(findId, findName);
-		return new ResponseEntity<List>(result, HttpStatus.OK);
-    }
+//	@PostMapping("/find_id")
+//	public ResponseEntity<String> findId(Users user) throws Exception {    	
+//		Users selectedUser = userService.selectByEmail(user);
+//		String find_id = selectedUser.getUserId();
+//		// String findName = selectedUser.getName();
+//		// List<String> result = Arrays.asList(findId, findName);
+//		return new ResponseEntity<>(find_id, HttpStatus.OK);
+//    }
     
   //아이디&비밀번호 찾기
     @GetMapping("/find_password")
