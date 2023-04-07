108d6b7d519dc15356ec0bad5137f1f8f4f4b974
hyungrack-Choi <sonijoa@naver.com>
2023-04-07 11:29:14 +0900

    123

--- project/src/main/java/com/human/project/domain/OAuthAttributes.java
+++ project/src/main/java/com/human/project/domain/OAuthAttributes.java
@@ -1,6 +1,7 @@
 package com.human.project.domain;
 
 import java.util.Map;
+import java.util.Set;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -16,27 +17,73 @@ public class OAuthAttributes {
 	private String email;
 	private String picture;
 	private String id;
+	private String nickname;
+	private String accessToken;
+	private String registrationId;
 	
 	@Builder
 	public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email,
-			String picture, String id) {
+			String picture, String id, String nickname, String socialType) {
 		this.attributes = attributes;
 		this.nameAttributeKey = nameAttributeKey;
 		this.name = name;
 		this.email = email;
 		this.picture = picture;
 		this.id = id;
+		this.nickname = nickname;
+		this.accessToken = accessToken;
+		this.registrationId = registrationId;
 	}
 	
 
 	public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
-		
-		return ofKakao(userNameAttributeName, attributes);
-		
+		if("naver".equals(registrationId)) {
+			return ofNaver("id", attributes);
+        }
+		else if("kakao".equals(registrationId)) {
+			return ofKakao(userNameAttributeName, attributes);
+		}
+        else {
+        	return ofGoogle(userNameAttributeName, attributes);
+        }
+
 	}
 
 
 	private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+		System.out.println("ofKakao=="+attributes);
+		Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+		Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+		
+		log.info("kakaoAccount : " + kakaoAccount);
+		log.info("profile : " + profile);
+		log.info("thumbnail_image_url : " + profile.get("thumbnail_image_url"));
+
+		return OAuthAttributes.builder()
+							  .name((String) profile.get("nickname"))
+							  .email((String) kakaoAccount.get("email") )
+							  .id( String.valueOf( attributes.get(userNameAttributeName) ) )
+							  .picture((String) profile.get("thumbnail_image_url") )
+							  .attributes(attributes)
+							  .nameAttributeKey(userNameAttributeName)
+							  .build();
+	}
+
+	private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+		System.out.println("ofNaver=="+attributes);
+		Map<String, Object> naverAccount = (Map<String, Object>) attributes.get("response");
+
+
+		return OAuthAttributes.builder()
+							  .name((String) naverAccount.get("name"))
+							  .email((String) naverAccount.get("email") )
+							  .picture((String) naverAccount.get("profile_image") )
+							  .attributes(naverAccount)
+							  .nameAttributeKey(userNameAttributeName)
+							  .build();
+	}
+
+	private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
 		
 		Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
 		Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
@@ -54,7 +101,4 @@ public class OAuthAttributes {
 							  .nameAttributeKey(userNameAttributeName)
 							  .build();
 	}
-	
-
 }
-
